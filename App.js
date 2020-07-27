7b541eb42ab9fc882c23c9559c656b3507e7c94a
zeebraa00 <zeebraa00@gmail.com>
2020-07-28 06:43:19 +0900

    [edit] learn flex styling

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, YellowBox } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}></View>
+      <View style={styles.bluewView}></View>
     </View>
   );
 }
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  yellowView: {
+    flex: 3,
+    backgroundColor: "yellow"
+  },
+  bluewView: {
+    flex: 2,
+    backgroundColor: "brown"
   },
 });
