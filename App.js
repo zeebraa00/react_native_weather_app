d7d9d7024672b91ae63ad2671817b978a7c5878f
zeebraa00 <zeebraa00@gmail.com>
2020-07-28 22:00:44 +0900

    [add] Loading page

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback, YellowBox } from 'react-native';
+import Loading from './Loading';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={styles.bluewView}></View>
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yellowView: {
-    flex: 3,
-    backgroundColor: "yellow"
-  },
-  bluewView: {
-    flex: 2,
-    backgroundColor: "brown"
-  },
-});
