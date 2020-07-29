763144de55b09880e800ea9086e7c120d6b62d80
zeebraa00 <zeebraa00@gmail.com>
2020-07-29 15:18:52 +0900

    [add] 날씨 api 적용(openWeather)

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Alert} from 'react-native';
 import Loading from './Loading';
-import { StyleSheet, Text, View} from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "181223d7949376695813e74a979326fe"; // openweathermap API_KEY
+
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+  getWeather = async (latitude, longitude) => {
+    const {data} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  }
+  getLocation = async() => {
+    try {
+      // throw Error;
+      await Location.requestPermissionsAsync();
+      const {
+        coords: {latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading : false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
