f000d80d069ebf30866b06d870901fa91c0c189e
zeebraa00 <zeebraa00@gmail.com>
2020-07-30 20:54:12 +0900

    [edit] 현재 위치의 온도 받아오기

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Alert} from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
 import * as Location from 'expo-location';
 import axios from 'axios';
 
@@ -10,12 +11,15 @@ export default class extends React.Component {
   state = {
     isLoading : true
   };
+
   getWeather = async (latitude, longitude) => {
     const {data} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    console.log(data);
-  }
+    console.log(data)
+    this.setState({ isLoading : false, temp : data.main.temp })
+  };
+
   getLocation = async() => {
     try {
       // throw Error;
@@ -24,7 +28,6 @@ export default class extends React.Component {
         coords: {latitude, longitude}
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading : false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
@@ -33,6 +36,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
---
+++ Weather.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Weather({ temp }) {
+    return (
+        <View style={styles.container}>
+            <Text>
+                { temp }
+            </Text>
+        </View>
+    )
+}
+
+Weather.propTypes = {
+    temp : PropTypes.number.isRequired
+};
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
